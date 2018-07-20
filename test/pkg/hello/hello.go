5f17c9fd19a9061eb5c55e1b56558bd5397900a2
Filip Nikolovski <filip.nikolovski@inplayer.com>
2018-07-20 16:33:51 +0200

    conflicts

--- test/pkg/hello/hello.go
+++ test/pkg/hello/hello.go
@@ -7,6 +7,10 @@ import (
 )
 
 func Hello(shape geometry.Shape) {
+<<<<<<< HEAD
 	fmt.Printf("asdad area: %f\n", shape.Area())
+=======
+	fmt.Printf("Sssehape area: %f\n", shape.Area())
+>>>>>>> d976ea68801608481c7c14cc5403136a55f95cde
 	fmt.Printf("Shape perimeter: %f\n", shape.Perim())
 }
