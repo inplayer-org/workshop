b6cc34d721f240555844f51413b3f9b1fc3297eb
DinoShambar <dinoshambar@gmail.com>
2018-08-16 11:59:31 +0200

    Improved the AddFood handler to return correct status codes

--- Unsolved_Problems/AnimalKingdom/src/main/FoodsApi.go
+++ Unsolved_Problems/AnimalKingdom/src/main/FoodsApi.go
@@ -32,6 +32,35 @@ func GetFood(w http.ResponseWriter, req *http.Request){
 	w.WriteHeader(http.StatusNotFound)
 }
 
+func AddFood(w http.ResponseWriter,req *http.Request){
+
+	var food structures.Food
+	json.NewDecoder(req.Body).Decode(&food)
+
+	//Check if the food id is already present in the database
+	for _,checkFood := range foods{
+		if food.FoodID == checkFood.FoodID{
+			w.WriteHeader(http.StatusAlreadyReported)
+			json.NewEncoder(w).Encode(checkFood)
+			return
+		}
+	}
+
+	w.WriteHeader(http.StatusAccepted)
+	foods = append(foods,food)
+	json.NewEncoder(w).Encode(food)
+
+}
+
+func DeleteFood(w http.ResponseWriter,req *http.Request){
+
+/*	var food structures.Food
+
+	param := mux.Vars(req)
+
+	for()*/
+}
+
 
 func main() {
 
@@ -39,7 +68,7 @@ func main() {
 
 
 	//Mock object
-	foods = append(foods,(structures.Food{FoodID:1,Type:"Herb",Name:"Grass"}))
+	foods = append(foods,structures.Food{FoodID:1,Type:"Herb",Name:"Grass"})
 	foods = append(foods,structures.Food{FoodID:2,Type:"Meat",Name:"Rabbit"})
 	foods = append(foods,structures.Food{FoodID:3,Type:"Herb",Name:"Leaves"})
 	foods = append(foods,structures.Food{FoodID:4,Type:"Meat",Name:"Deer"})
@@ -49,6 +78,7 @@ func main() {
 	//Handlers
 	router.HandleFunc("/foods",GetFoods).Methods("GET")
 	router.HandleFunc("/foods/{id}",GetFood).Methods("GET")
+	router.HandleFunc("/foods/{id}",AddFood).Methods("POST")
 
 
 	http.ListenAndServe(":8889", router)
