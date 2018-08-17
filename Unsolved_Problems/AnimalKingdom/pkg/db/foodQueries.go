2dc3faa65a310545c87873849b550ab1d7fa3762
DinoShambar <dinoshambar@gmail.com>
2018-08-17 16:36:11 +0200

    Split Queries in db package from db.go file to foodQueries.go file

--- Unsolved_Problems/AnimalKingdom/pkg/db/foodQueries.go
+++ Unsolved_Problems/AnimalKingdom/pkg/db/foodQueries.go
@@ -1,1 +1,77 @@
 package db
+
+import (
+	"database/sql"
+	"repo.inplayer.com/workshop/Unsolved_Problems/AnimalKingdom/pkg/structures"
+	"log"
+)
+
+//SelectAllFood return slice of all food
+func SelectAllFood(db *sql.DB) []structures.Food {
+	rows, err := db.Query("SELECT * FROM Food")
+	errorHandler(err)
+	listOfFood := []structures.Food{}
+	for rows.Next() {
+		var typeFood, name string
+		var ID int
+		food := structures.Food{}
+		err := rows.Scan(&ID, &typeFood,&name)
+		errorHandler(err)
+		food.FoodID = ID
+		food.Name = name
+		food.Type = typeFood
+		listOfFood = append(listOfFood, food)
+	}
+	return listOfFood
+}
+
+
+func SelectFoodbyName(db *sql.DB, foodName string) interface{} {
+	var typeFood, name string
+	var ID int
+	err := db.QueryRow("SELECT * FROM Food WHERE foodName=(?)", foodName).Scan(&ID, &typeFood,&name)
+	if errorExistant(err){
+		return nil
+	}
+	food := structures.Food{FoodID:ID,Name:name,Type:typeFood}
+	food.FoodID = ID
+	food.Name = name
+	food.Type = typeFood
+	return structures.Food{FoodID:ID,Name:name,Type:typeFood}
+}
+
+func DeleteFoodbyName(db *sql.DB, foodName string) error{
+	delFood, err := db.Prepare("DELETE FROM Food WHERE foodName=(?)")
+	errorHandler(err)
+	_,err = delFood.Exec(db,foodName)
+	return err
+}
+
+func DeleteFoodbyID(db *sql.DB, foodID int) error{
+	delFood, err := db.Prepare("DELETE FROM Food WHERE foodID=(?)")
+	errorHandler(err)
+	_,err = delFood.Exec(foodID)
+	log.Println(err)
+	return err
+}
+
+func InsertFood(db *sql.DB, food structures.Food) {
+	f := food
+	_, err := db.Exec("INSERT INTO Food(foodID,foodName,type)  VALUES (?,?,?)",f.FoodID, f.Name, f.Type)
+	errorHandler(err)
+}
+
+func SelectAllFoodByType(db *sql.DB, typeFood string) []string {
+	rows, err := db.Query("SELECT foodName, count(foodName) FROM Food WHERE type=(?) GROUP BY foodName", typeFood)
+	errorHandler(err)
+	var food []string
+	for rows.Next() {
+		var name string
+		var count int
+		err := rows.Scan(&name, &count)
+		errorHandler(err)
+		food = append(food, name)
+	}
+	return food
+
+}
