487d2895d4f48766fb19f5af7a34ec353652bdb0
Gligor23 <glgudjevski@gmail.com>
2018-08-20 09:15:57 +0200

    position removed from all struct to position.go

---
+++ Unsolved_Problems/jsonExample/pkg/employerinfo/position.go
@@ -0,0 +1,96 @@
+package employerinfo
+
+import (
+	"fmt"
+	"database/sql"
+	"repo.inplayer.com/workshop/Unsolved_Problems/jsonExample/pkg/errorhandle"
+)
+
+type Position struct {
+
+	Name string `json:"name"`
+	Description string `json:"description"`
+
+}
+
+func (p *Position) Get(db *sql.DB) error {
+
+	query := fmt.Sprintf("SELECT emp_position, description FROM position_info WHERE emp_position='%s'",p.Name)
+	return db.QueryRow(query).Scan(&p.Name,&p.Description)
+
+}
+
+func (p *Position) Create(db *sql.DB) error{
+	err:= errorhandle.CheckString(&p.Name)
+
+	if err != nil {
+		//fmt.Println(err)
+		return err
+	}
+
+	query:=fmt.Sprintf("INSERT INTO position_info(emp_position,description) VALUES('%s','%s')",p.Name,p.Description)
+	_,err=db.Exec(query)
+	return err
+
+
+}
+
+func (p *Position) Update(db *sql.DB) error{
+	err:= errorhandle.CheckString(&p.Name)
+
+	if err != nil {
+		//fmt.Println(err)
+		return err
+	}
+	query:=fmt.Sprintf("UPDATE position_info SET description='%s' WHERE emp_position='%s'",p.Description,p.Name)
+	_,err =db.Exec(query)
+	return err
+
+}
+
+func (p *Position) Delete(db *sql.DB) error{
+
+	query:=fmt.Sprintf("DELETE FROM contract WHERE emp_position='%s'",p.Name)
+	_,err:=db.Exec(query)
+
+	if err != nil {
+		return err
+	}
+
+	query=fmt.Sprintf("DELETE FROM position_info WHERE emp_position='%s'",p.Name)
+	_,err=db.Exec(query)
+	return err
+
+}
+
+func GetAllPositions(db *sql.DB)([]Position,error){
+
+	query:=fmt.Sprintf("SELECT emp_position,description FROM position_info")
+	rows,err:=db.Query(query)
+
+	if err!=nil {
+		return nil,err
+	}
+
+	defer rows.Close()
+
+	return rowsToPositions(rows)
+}
+
+func rowsToPositions(rows *sql.Rows)([]Position,error){
+	var positions  []Position
+
+	for rows.Next() {
+		var p Position
+		err:=rows.Scan(&p.Name,&p.Description)
+
+		if err!=nil {
+			return nil,err
+		}
+
+		positions=append(positions,p)
+	}
+
+	return positions,nil
+}
+
