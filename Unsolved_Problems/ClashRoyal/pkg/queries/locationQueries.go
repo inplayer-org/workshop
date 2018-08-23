1f92ef1a09e4b616f8e3bdeeffbdeaef656208b4
Gligor23 <glgudjevski@gmail.com>
2018-08-23 11:51:17 +0200

    fuck off

--- Unsolved_Problems/ClashRoyal/pkg/queries/locationQueries.go
+++ Unsolved_Problems/ClashRoyal/pkg/queries/locationQueries.go
@@ -4,39 +4,12 @@ import (
 	"database/sql"
 	"repo.inplayer.com/workshop/Unsolved_Problems/ClashRoyal/pkg/locations"
 	_ "github.com/go-sql-driver/mysql"
-	"strconv"
 )
 
-//UpdateLocations if the location exists in database then it updates it if it doeesnt then it inserts it
-func UpdateLocations(db *sql.DB,locs locations.Locations)error{
 
-	for _,elem:=range locs.Location {
-
-		if Exists(db, "locations", "id", strconv.Itoa(elem.ID)) {
-
-			err := insertIntoDB(db,elem.ID, elem.Name, elem.IsCountry, elem.CountryCode)
-
-			if err != nil {
-				return err
-			}
-
-		} else {
-
-			err := updateDB(db,elem.ID, elem.Name, elem.IsCountry, elem.CountryCode)
-
-			if err != nil {
-				return err
-			}
-
-		}
-	}
-
-	return nil
-
-}
 
 //insertInto inserts location into database
-func insertIntoDB(db *sql.DB,id int,name string,isCountry bool,code string)error{
+func InsertIntoLocationsTable(db *sql.DB,id int,name string,isCountry bool,code string)error{
 
 	_, err := db.Exec("INSERT INTO locations(id,countryName,isCountry,countryCode) VALUES ((?).(?),(?),(?));", id, name, isCountry, code)
 
@@ -50,7 +23,7 @@ func insertIntoDB(db *sql.DB,id int,name string,isCountry bool,code string)error
 
 
 //update for an id updates a location
-func updateDB(db *sql.DB,id int,name string,isCountry bool,code string)error{
+func UpdateLocationsTable(db *sql.DB,id int,name string,isCountry bool,code string)error{
 
 	_, err := db.Exec("UPDATE locations SET countryName=(?),isCounrty=(?),countryCode=(?) WHERE id=(?)", name, isCountry, code, id)
 
@@ -75,4 +48,30 @@ func GetLocationID(db *sql.DB,name string)(int,error){
 
 	return id ,nil
 
+}
+
+//GetAllLocations returns all locations from locations table in database
+func GetAllLocations(db *sql.DB)(locations.Locations,error){
+
+	var locs locations.Locations
+
+	rows,err:=db.Query("SELECT (id,countryName,isCountry,countryCode) FROM locations;")
+
+	if err !=nil {
+		return locs,err
+	}
+
+	i:=0
+	for rows.Next(){
+		err:=rows.Scan(&locs.Location[i].ID,&locs.Location[i].Name,&locs.Location[i].IsCountry,&locs.Location[i].CountryCode)
+
+		if err!=nil {
+			return locs,err
+		}
+
+		i++
+
+		}
+
+	return locs,nil
 }
--- Unsolved_Problems/ClashRoyal/pkg/update/updateclans.go
+++ Unsolved_Problems/ClashRoyal/pkg/update/updateclans.go
@@ -3,6 +3,9 @@ package update
 import (
 	"database/sql"
 	"repo.inplayer.com/workshop/Unsolved_Problems/ClashRoyal/pkg/structures"
+	"repo.inplayer.com/workshop/Unsolved_Problems/ClashRoyal/pkg/locations"
+	"strconv"
+	"repo.inplayer.com/workshop/Unsolved_Problems/ClashRoyal/pkg/queries"
 )
 
 
@@ -25,3 +28,31 @@ for _,elem:=range clan {
 
  return err
  }
+
+//UpdateLocations if the location exists in database then it updates it if it doeesnt then it inserts it
+func UpdateLocations(db *sql.DB,locs locations.Locations)error{
+
+	for _,elem:=range locs.Location {
+
+		if queries.Exists(db, "locations", "id", strconv.Itoa(elem.ID)) {
+
+			err := queries.InsertIntoLocationsTable(db,elem.ID, elem.Name, elem.IsCountry, elem.CountryCode)
+
+			if err != nil {
+				return err
+			}
+
+		} else {
+
+			err := queries.UpdateLocationsTable(db,elem.ID, elem.Name, elem.IsCountry, elem.CountryCode)
+
+			if err != nil {
+				return err
+			}
+
+		}
+	}
+
+	return nil
+
+}
