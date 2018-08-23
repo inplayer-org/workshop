c10e223246314a8767d5ecef9ace3467e7a520a5
Gligor23 <glgudjevski@gmail.com>
2018-08-23 09:44:19 +0200

    GetLocatonID returns the id for a locationName

--- Unsolved_Problems/ClashRoyal/pkg/routeranddb/routeranddb.go
+++ Unsolved_Problems/ClashRoyal/pkg/routeranddb/routeranddb.go
@@ -16,17 +16,24 @@ type App struct {
 }
 
 func (a *App) Initialize(user, password, dbname string) {
+
 	connectionString := fmt.Sprintf("%s:%s@/%s", user, password, dbname)
+
 	var err error
-	a.DB, err = sql.Open("mysql", connectionString)
+	a.DB,err = sql.Open("mysql", connectionString)
+
 	if err != nil {
 		log.Fatal(err)
-	}
 
+	}
 
 	a.Router = mux.NewRouter()
 //	a.initializeRoutes()
 }
 
 func (a *App) Run(addr string) {
+
 	log.Fatal(http.ListenAndServe(addr, a.Router))
+
+}
+
