87dd7033a207e7133696847f4ff6ba55adec9158
DinoShambar <dinoshambar@gmail.com>
2018-08-29 13:30:27 +0200

    Prototype version for intert/update player with nil values as clanTag and locationID

--- Unsolved_Problems/ClashRoyal/pkg/queries/players.go
+++ Unsolved_Problems/ClashRoyal/pkg/queries/players.go
@@ -6,43 +6,45 @@ import (
 )
 
 
-func update(DB *sql.DB,player structures.PlayerStats,locationID int)error{
-	if locationID==0{
-		_,err := DB.Exec("update players SET playerName=(?),wins=(?),losses=(?),trophies=(?),clanTag=(?) where playerTag=(?);",
-			player.Name, player.Wins, player.Losses, player.Trophies, player.Clan.Tag, player.Tag)
-		UpdateError(err)
-		return err
-	}else{
+func update(DB *sql.DB,player structures.PlayerStats,locationID interface{},clanTag interface{})error{
+
 		_,err := DB.Exec("update players SET playerName=(?),wins=(?),losses=(?),trophies=(?),clanTag=(?),locationID=(?) where playerTag=(?);",
-			player.Name, player.Wins, player.Losses, player.Trophies, player.Clan.Tag, locationID,player.Tag)
+			player.Name, player.Wins, player.Losses, player.Trophies, clanTag, locationID,player.Tag)
 		UpdateError(err)
 		return err
-	}
+
 }
 
-func insert(DB *sql.DB,player structures.PlayerStats,locationID int) error{
-	if locationID == 0{
-		_,err := DB.Exec(`INSERT INTO players(playerTag,playerName,wins,losses,trophies,clanTag) values((?),(?),(?),(?),(?),(?));`,
-			player.Tag, player.Name, player.Wins, player.Losses, player.Trophies, player.Clan.Tag)
-		InsertError(err)
-		return err
-	}else {
+func insert(DB *sql.DB,player structures.PlayerStats,locationID interface{},clanTag interface{}) error{
+
 		_, err := DB.Exec(`INSERT INTO players(playerTag,playerName,wins,losses,trophies,clanTag,locationID) values((?),(?),(?),(?),(?),(?),(?));`,
-			player.Tag, player.Name, player.Wins, player.Losses, player.Trophies, player.Clan.Tag, locationID)
+			player.Tag, player.Name, player.Wins, player.Losses, player.Trophies, clanTag, locationID)
 		InsertError(err)
 		return err
-	}
 }
 
 func UpdatePlayer(DB *sql.DB,player structures.PlayerStats,locationID int)error{
 
+	var clanTag interface{}
+	var locID interface{}
+
+	clanTag = nil
+	locID = nil
+
 	//log.Println("Updating for player ",player)
 	if !(player.Clan.Tag=="") && !(player.Clan.Name==""){
 		UpdateClans(DB,structures.Clan{Tag:player.Clan.Tag,Name:player.Clan.Name})
+		clanTag = player.Clan.Tag
 	}
+
+	if locationID!=0{
+		locID = locationID
+	}
+
+
 	if Exists(DB,PlayersTable,PlayerTag,player.Tag){
-		return update(DB,player,locationID)
+		return update(DB,player,locID,clanTag)
 	}else {
-		return insert(DB,player,locationID)
+		return insert(DB,player,locID,clanTag)
 	}
 }
