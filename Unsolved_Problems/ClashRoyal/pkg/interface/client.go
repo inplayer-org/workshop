2182120abcae5d698bd948279009e3012ef4e9ae
Gligor23 <glgudjevski@gmail.com>
2018-09-05 11:46:10 +0200

    asd

--- Unsolved_Problems/ClashRoyal/pkg/interface/client.go
+++ Unsolved_Problems/ClashRoyal/pkg/interface/client.go
@@ -38,7 +38,7 @@ func SetHeaders(req *http.Request){
 	req.Header.Add("Content-Type","application/json")
 	req.Header.Add("authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjBkMTUxODQ4LWM0ZTgtNGU1Zi05NzRiLWQzNjQ1ZjAxMzk2MiIsImlhdCI6MTUzNDg1NDQ2MCwic3ViIjoiZGV2ZWxvcGVyL2U1ODJhZWJlLWNlNGUtNGVhMC1hZTgwLTk5MTdhMmNkMGZhYyIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI2Mi4xNjIuMTY4LjE5NCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.8-GoA48DGZScCOi6EU4AAuJUcXbY2kqqHwsEXg22w4hDHJegjuSaS6jjDSoZcZFSS9x6Fbkd825eSagpAjbX4Q")
 }
-func (c *MyClient) GetRequestForPlayer (db *sql.DB,tag string) (int,error) {
+func (c *MyClient) GetRequestForPlayer (tag string) (int,error) {
 
 	var currentPlayer structures.PlayerStats
 
@@ -64,7 +64,7 @@ func (c *MyClient) GetRequestForPlayer (db *sql.DB,tag string) (int,error) {
 
 			json.NewDecoder(resp.Body).Decode(&currentPlayer)
 			currentPlayer.Tag = "#"+currentPlayer.Tag[1:]
-			queries.UpdatePlayer(db,currentPlayer,0)
+			queries.UpdatePlayer(c.db,currentPlayer,0)
 
 			break
 		}
