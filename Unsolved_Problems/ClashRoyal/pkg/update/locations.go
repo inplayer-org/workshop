de12b3a0fba35ed73ed3e8e6e50b9ca86f1f0c76
ElenaPishovska <elenapisovska@yahoo.com>
2018-09-06 22:02:31 +0200

    Modified channels in locations.go with waitGroup

--- Unsolved_Problems/ClashRoyal/pkg/update/locations.go
+++ Unsolved_Problems/ClashRoyal/pkg/update/locations.go
@@ -2,73 +2,77 @@ package update
 
 import (
 	"database/sql"
-	"fmt"
+	"strconv"
+	"sync"
+
+	"repo.inplayer.com/workshop/Unsolved_Problems/ClashRoyal/pkg/interface"
 	"repo.inplayer.com/workshop/Unsolved_Problems/ClashRoyal/pkg/queries"
 	"repo.inplayer.com/workshop/Unsolved_Problems/ClashRoyal/pkg/structures"
-	"strconv"
-	"time"
-	"repo.inplayer.com/workshop/Unsolved_Problems/ClashRoyal/pkg/interface"
 )
 
-//UpdateLocations if the location exists in database then it updates it if it doeesnt then it inserts it
-func Locations(db *sql.DB,locs structures.Locations)error {
+var wg sync.WaitGroup
 
-	var err error
+//UpdateLocations if the location exists in database then it updates it if it doeesnt then it inserts it
+func Locations(db *sql.DB, locs structures.Locations) error {
+
+	//var err error
 	done := make(chan error)
-	defer close(done)
-	responsesCount := 0
+	// defer close(done)
+	// responsesCount := 0
 
 	for _, elem := range locs.Location {
-		responsesCount++
-		go CurrentLocation(db,elem,done)
-		time.Sleep(time.Millisecond*15)
+		//responsesCount++
+		go CurrentLocation(db, elem, done)
+		//time.Sleep(time.Millisecond*15)
 	}
+	wg.Wait()
+	close(done)
 
-	for ; responsesCount > 0; responsesCount-- {
-		err = <-done
+	// for ; responsesCount > 0; responsesCount-- {
+	// 	err = <-done
 
-		if err!=nil{
+	// 	if err != nil {
+	// 		return err
+	// 	}
+
+	// }
+	for err := range done {
+		if err != nil {
 			return err
 		}
-
 	}
 
 	return nil
 }
 
-func CurrentLocation(db *sql.DB,elem structures.Locationsinfo,done chan <- error){
+func CurrentLocation(db *sql.DB, elem structures.Locationsinfo, done chan<- error) {
 	var err error
-	for {
-		if !queries.Exists(db, "locations", "id", strconv.Itoa(elem.ID)) {
-			err = queries.InsertIntoLocationsTable(db, elem.ID, elem.Name, elem.IsCountry, elem.CountryCode)
-		} else {
-			err = queries.UpdateLocationsTable(db, elem.ID, elem.Name, elem.IsCountry, elem.CountryCode)
-		}
-		if err==nil{
-			break
-		}
-		fmt.Println("Error for ",elem.Name," -> ",err)
-	}
-		//log.Println("Finished for -> ",elem.Name)
-		done<-err
+
+	if !queries.Exists(db, "locations", "id", strconv.Itoa(elem.ID)) {
+		err = queries.InsertIntoLocationsTable(db, elem.ID, elem.Name, elem.IsCountry, elem.CountryCode)
+	} else {
+		err = queries.UpdateLocationsTable(db, elem.ID, elem.Name, elem.IsCountry, elem.CountryCode)
 	}
 
-//DailyUpdateLocation makes request and updates the location table
-func DailyUpdate(db *sql.DB)(structures.Locations,error){
-	client := _interface.NewClient()
-	locations,err:=client.GetLocations()
-
-	if err!=nil{
-		return locations,err
-	}
-
-	err=Locations(db,locations)
-
-	if err!=nil{
-		return locations,err
-	}
-
-	return locations,nil
-
+	//log.Println("Finished for -> ",elem.Name)
+	done <- err
 }
 
+//DailyUpdateLocation makes request and updates the location table
+func DailyUpdate(db *sql.DB) (structures.Locations, error) {
+	client := _interface.NewClient()
+	locations, err := client.GetLocations()
+
+	if err != nil {
+		return locations, err
+	}
+
+	err = Locations(db, locations)
+
+	if err != nil {
+		return locations, err
+	}
+
+	return locations, nil
+
+}
