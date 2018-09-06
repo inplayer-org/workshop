fd5f2082f9b498cf01bb2f41b399ba5ecae9c999
ElenaPishovska <elenapisovska@yahoo.com>
2018-09-06 22:07:57 +0200

    IMproved locations.go with adding waitGroup

--- Unsolved_Problems/ClashRoyal/pkg/update/locations.go
+++ Unsolved_Problems/ClashRoyal/pkg/update/locations.go
@@ -15,27 +15,14 @@ var wg sync.WaitGroup
 //UpdateLocations if the location exists in database then it updates it if it doeesnt then it inserts it
 func Locations(db *sql.DB, locs structures.Locations) error {
 
-	//var err error
 	done := make(chan error)
-	// defer close(done)
-	// responsesCount := 0
 
 	for _, elem := range locs.Location {
-		//responsesCount++
 		go CurrentLocation(db, elem, done)
-		//time.Sleep(time.Millisecond*15)
 	}
 	wg.Wait()
 	close(done)
 
-	// for ; responsesCount > 0; responsesCount-- {
-	// 	err = <-done
-
-	// 	if err != nil {
-	// 		return err
-	// 	}
-
-	// }
 	for err := range done {
 		if err != nil {
 			return err
@@ -47,14 +34,13 @@ func Locations(db *sql.DB, locs structures.Locations) error {
 
 func CurrentLocation(db *sql.DB, elem structures.Locationsinfo, done chan<- error) {
 	var err error
-
+	defer wg.Done()
 	if !queries.Exists(db, "locations", "id", strconv.Itoa(elem.ID)) {
 		err = queries.InsertIntoLocationsTable(db, elem.ID, elem.Name, elem.IsCountry, elem.CountryCode)
 	} else {
 		err = queries.UpdateLocationsTable(db, elem.ID, elem.Name, elem.IsCountry, elem.CountryCode)
 	}
 
-	//log.Println("Finished for -> ",elem.Name)
 	done <- err
 }
 
