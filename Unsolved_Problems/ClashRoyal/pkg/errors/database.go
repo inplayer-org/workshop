e9204df2df2fbc2179a094936e5d2090163d5396
DinoShambar <dinoshambar@gmail.com>
2018-09-10 12:45:17 +0200

    Added Database file in errors package for handling errors from opening database and using queries

--- Unsolved_Problems/ClashRoyal/pkg/errors/database.go
+++ Unsolved_Problems/ClashRoyal/pkg/errors/database.go
@@ -1,1 +1,24 @@
 package errors
+
+import (
+	"database/sql"
+	"github.com/pkg/errors"
+	"log"
+)
+
+func Database(err error)error{
+	if err==sql.ErrNoRows{
+		newErr := errors.Errorf("ERROR ErrNoRows: %s",err)
+		log.Println(newErr)
+		return err
+	}else if err==sql.ErrConnDone{
+		newErr := errors.Errorf("ERROR ErrConnDone: %s",err)
+		log.Println(newErr)
+		return err
+	}else if err!=nil{
+		newErr := errors.Errorf("ERROR Undefined: %s",err)
+		log.Println(newErr)
+		return err
+	}
+	return nil
+}
