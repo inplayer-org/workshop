910fefaa055fe8d0eeb4f49218897f194e373691
DinoShambar <dinoshambar@gmail.com>
2018-09-13 09:44:32 +0200

    Unused function to Hash as a testing example

--- Unsolved_Problems/ClashRoyal/pkg/parser/tags.go
+++ Unsolved_Problems/ClashRoyal/pkg/parser/tags.go
@@ -1,5 +1,7 @@
 package parser
 
+import "errors"
+
 //ToUrlTag - Removes the first character (should be #) and concatenates %25 in from for making a request from the Clash Royal api by tag for a single string entry
 func ToRequestTag(tag string) string{
 
@@ -17,4 +19,32 @@ func ToRawTag(tag string)string {
 
 	return tag[1:]
 
+}
+
+func FromAnyToHashTag(tag string)(string,error){
+	if len(tag) == 0{
+		return "",errors.New("tried to convert empty string")
+	}
+	for {
+		if len(tag) ==0{
+			return "",errors.New("string is only consisted of #, %25,% and &")
+		}
+		switch string(tag[0]) {
+		case "#":
+			tag=tag[1:]
+			break
+		case "%":
+			if tag[:3]=="%25"{
+				tag = tag[3:]
+			}else {
+				tag=tag[1:]
+			}
+			break
+		case "&":
+			tag=tag[1:]
+			break
+		default:
+			return "#"+tag,nil
+		}
+	}
 }
