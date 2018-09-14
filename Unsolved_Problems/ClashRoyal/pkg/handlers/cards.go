717a1ac684a54a56fcbe1c6e5273142dccc51955
Darko Djalevski <dare_zalevski@yahoo.com>
2018-09-14 16:25:30 +0200

    added function to search card from db with name

--- Unsolved_Problems/ClashRoyal/pkg/handlers/cards.go
+++ Unsolved_Problems/ClashRoyal/pkg/handlers/cards.go
@@ -5,6 +5,7 @@ import (
 	"repo.inplayer.com/workshop/Unsolved_Problems/ClashRoyal/tmpl"
 	"repo.inplayer.com/workshop/Unsolved_Problems/ClashRoyal/pkg/errors"
 	"repo.inplayer.com/workshop/Unsolved_Problems/ClashRoyal/pkg/cards"
+	"github.com/gorilla/mux"
 )
 
 func (a *App) GetCards(w http.ResponseWriter, r *http.Request) {
@@ -20,3 +21,22 @@ func (a *App) GetCards(w http.ResponseWriter, r *http.Request) {
 
 }
 
+
+
+// Get card by name from DB
+func (a *App) GetCardByName (w http.ResponseWriter, r *http.Request){
+
+	vars := mux.Vars(r)
+	name := vars["name"]
+
+	//querry for get card by name
+	cardsLike,err := cards.GetCardLike(a.DB,name)
+
+	if err != nil {
+		tmpl.Tmpl.ExecuteTemplate(w,"error.html",errors.NewResponseError("Card Name doesn't exist","There is no card name like "+name,404))
+		return
+	}
+
+	tmpl.Tmpl.ExecuteTemplate(w,"bynamecards.html",cardsLike)
+
+}
