c3f3e2518dbebf3762e8ff3939f44677483ca4c6
ElenaPishovska <elenapisovska@yahoo.com>
2018-09-17 12:51:29 +0200

    Modified location handlers and new template for locations

--- Unsolved_Problems/ClashRoyal/pkg/handlers/locations.go
+++ Unsolved_Problems/ClashRoyal/pkg/handlers/locations.go
@@ -1,49 +1,72 @@
 package handlers
 
-
-
 import (
 	"net/http"
+
 	"github.com/gorilla/mux"
 
 	"strconv"
-	"repo.inplayer.com/workshop/Unsolved_Problems/ClashRoyal/tmpl"
+
 	"repo.inplayer.com/workshop/Unsolved_Problems/ClashRoyal/pkg/errors"
-	"repo.inplayer.com/workshop/Unsolved_Problems/ClashRoyal/pkg/rankedPlayer"
 	"repo.inplayer.com/workshop/Unsolved_Problems/ClashRoyal/pkg/locations"
+	"repo.inplayer.com/workshop/Unsolved_Problems/ClashRoyal/pkg/parser"
+	"repo.inplayer.com/workshop/Unsolved_Problems/ClashRoyal/pkg/rankedPlayer"
+	"repo.inplayer.com/workshop/Unsolved_Problems/ClashRoyal/tmpl"
 )
+
 // Get location by name and return top250 rankedPlayer in that location by wins
-func (a *App) GetLocationByName (w http.ResponseWriter, r *http.Request){
+func (a *App) GetLocationByName(w http.ResponseWriter, r *http.Request) {
 
 	vars := mux.Vars(r)
 	name := vars["name"]
-	id,err:=strconv.Atoi(name)
+	id, err := strconv.Atoi(name)
 
-	if err !=nil {
-		tmpl.Tmpl.ExecuteTemplate(w,"error.html",errors.NewResponseError("Location ID not number","ID should contain only numbers",404))
+	if err != nil {
+		tmpl.Tmpl.ExecuteTemplate(w, "error.html", errors.NewResponseError("Location ID not number", "ID should contain only numbers", 404))
 		return
 	}
-// querry from DB to list and sort 250 rankedPlayer from 1 location
-	player,_ := rankedPlayer.GetPlayersByLocation(a.DB,id)
+	// querry from DB to list and sort 250 rankedPlayer from 1 location
+	player, _ := rankedPlayer.GetPlayersByLocation(a.DB, id)
 
-		if len(player)==0 {
-			tmpl.Tmpl.ExecuteTemplate(w,"error.html",errors.NewResponseError("Location not found","Location with "+strconv.Itoa(id)+" doesnot exist",404))
-			return
+	if len(player) == 0 {
+		tmpl.Tmpl.ExecuteTemplate(w, "error.html", errors.NewResponseError("Location not found", "Location with "+strconv.Itoa(id)+" doesnot exist", 404))
+		return
 	}
 
-	tmpl.Tmpl.ExecuteTemplate(w,"tableranking.html",player)
+	tmpl.Tmpl.ExecuteTemplate(w, "tableranking.html", player)
 
 }
+
+// // Returning all locations from DB
+// func (a *App) GetLocations(w http.ResponseWriter, r *http.Request) {
+// // listing(returning) all locations from DB
+// 	locs, err := locations.GetAllLocations(a.DB)
+
+// 	if err != nil {
+// 		tmpl.Tmpl.ExecuteTemplate(w,"error.html",errors.NewResponseError("Server error","Can't load locations something went wrong",503))
+// 		return
+// 	}
+
+// 	tmpl.Tmpl.ExecuteTemplate(w,"locs.html",locs)
+
+// }
+
 // Returning all locations from DB
 func (a *App) GetLocations(w http.ResponseWriter, r *http.Request) {
-// listing(returning) all locations from DB
+	// listing(returning) all locations from DB
 	locs, err := locations.GetAllLocations(a.DB)
 
 	if err != nil {
-		tmpl.Tmpl.ExecuteTemplate(w,"error.html",errors.NewResponseError("Server error","Can't load locations something went wrong",503))
+		tmpl.Tmpl.ExecuteTemplate(w, "error.html", errors.NewResponseError("Server error", "Can't load locations something went wrong", 503))
 		return
 	}
-
-	tmpl.Tmpl.ExecuteTemplate(w,"locs.html",locs)
+	m := make(map[string][]string)
+	for _, j := range locs {
+		if j.IsCountry {
+			key, _ := parser.FirstCharFrom(j.Name)
+			m[key] = append(m[key], j.Name)
+		}
+	}
+	tmpl.Tmpl.ExecuteTemplate(w, "locationsNew.html", m)
 
 }
