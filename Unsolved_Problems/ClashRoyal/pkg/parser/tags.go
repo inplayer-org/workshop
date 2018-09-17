80c0d3853d0f60d307a4a334dfc214fd02836edd
Darko Djalevski <dare_zalevski@yahoo.com>
2018-09-17 13:46:15 +0200

    fix parser error for dailyupdates

--- Unsolved_Problems/ClashRoyal/pkg/parser/tags.go
+++ Unsolved_Problems/ClashRoyal/pkg/parser/tags.go
@@ -9,7 +9,7 @@ import (
 //ToUrlTag - Removes the first character (should be #) and concatenates %25 in from for making a request from the Clash Royal api by tag for a single string entry
 func ToRequestTag(tag string) string {
 
-	return "%25a" + tag[1:]
+	return "%25" + tag[1:]
 
 }
 
