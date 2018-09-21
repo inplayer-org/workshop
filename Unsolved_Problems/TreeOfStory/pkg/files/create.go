fab1900284e110a21eb860147ae0ade9324215b9
Darko Djalevski <dare_zalevski@yahoo.com>
2018-09-21 14:52:26 +0200

    extendet create files functions

--- Unsolved_Problems/TreeOfStory/pkg/files/create.go
+++ Unsolved_Problems/TreeOfStory/pkg/files/create.go
@@ -2,6 +2,8 @@ package files
 
 import (
 	"os"
+	"fmt"
+	"io"
 )
 
 
@@ -17,5 +19,60 @@ func CreateDirectory(path string) {
 
 
 
+func CreateFile(path string) {
+	// detect if file exists
+	var _, err = os.Stat(path)
+
+	// create file if not exists
+	if os.IsNotExist(err) {
+		var file, err = os.Create(path)
+		if isError(err) { return }
+		defer file.Close()
+	}
+
+	fmt.Println("==> done creating file", path)
+}
 
 
+func ReadFile(path string) {
+	// re-open file
+	var file, err = os.OpenFile(path, os.O_RDWR, 0644)
+	if isError(err) { return }
+	defer file.Close()
+
+	// read file, line by line
+	var text = make([]byte, 1024)
+	for {
+		_, err = file.Read(text)
+
+		// break if finally arrived at end of file
+		if err == io.EOF {
+			break
+		}
+
+		// break if error occured
+		if err != nil && err != io.EOF {
+			isError(err)
+			break
+		}
+	}
+
+	fmt.Println("==> done reading from file")
+	fmt.Println(string(text))
+}
+
+func DeleteFile(path string) {
+	// delete file
+	var err = os.Remove(path)
+	if isError(err) { return }
+
+	fmt.Println("==> done deleting file")
+}
+
+func isError(err error) bool {
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	return (err != nil)
+}
