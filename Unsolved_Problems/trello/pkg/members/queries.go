3b104532c83bdc0282b09020fa74fd760eba07fc
DIno <dinoshambar@gmail.com>
2018-09-26 10:11:40 +0200

    Created update by ID and Update by username functions for members

--- Unsolved_Problems/trello/pkg/members/queries.go
+++ Unsolved_Problems/trello/pkg/members/queries.go
@@ -2,7 +2,7 @@ package members
 
 import "database/sql"
 
-func InsertMember(DB *sql.DB,member Member)error{
+func Insert(DB *sql.DB,member Member)error{
 
 	_,err := DB.Exec("INSERT INTO Trello.Members (ID,fullname,initials,username,email) VALUES (?,?,?,?,?);",member.ID,member.FullName,member.Initials,member.Username,member.Email)
 
@@ -11,3 +11,22 @@ func InsertMember(DB *sql.DB,member Member)error{
 
 }
 
+func Update(DB *sql.DB,member Member){
+
+
+
+}
+
+func updateByID(DB *sql.DB,member Member)error{
+
+	_,err := DB.Exec("UPDATE Trello.Members SET fullname=?,initials=?,username=?,email=? WHERE ID=?",member.FullName,member.Initials,member.Username,member.Email,member.ID)
+
+	return err
+}
+
+func updateByUsername(DB *sql.DB,member Member)error{
+
+	_,err := DB.Exec("UPDATE Trello.Members SET ID=?,fullname=?,initials=?,email=? WHERE username=?",member.ID,member.FullName,member.Initials,member.Email,member.Username)
+
+	return err
+}
