2710220e14238ad84beb3976af6066868fc989f7
Gligor23 <glgudjevski@gmail.com>
2018-09-28 12:56:32 +0200

    fa

--- Unsolved_Problems/trello/pkg/bigBoard/queries.go
+++ Unsolved_Problems/trello/pkg/bigBoard/queries.go
@@ -1,8 +1,24 @@
 package bigBoard
 
-import "database/sql"
+import (
+	"database/sql"
+	"repo.inplayer.com/workshop/Unsolved_Problems/trello/pkg/errors"
+)
 
 func(bb *BigBoard)Insert(DB *sql.DB)error{
+
+	_,err:=DB.Exec("INSERT into Boards (ID,nameBoard,decrip,shortUrl) values (?,?,?,?);",bb.ID,bb.Name,bb.Desc,bb.ShortURL)
+
+	if err!=nil{
+		return errors.Database(err)
+	}
+
+	_,err=DB.Exec("INSERT into Lists (ID,nameList,IDboard) values (?,?,?);",bb.Lists.ID,bb.Lists.Name,bb.Lists.IDBoard)
+
+	if err!=nil{
+		return errors.Database(err)
+	}
+
 
 	return nil
 }
