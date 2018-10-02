fad3aed57d1a218eaed3b7bd8f5a8c134957a219
ElenaPishovska <elenapisovska@yahoo.com>
2018-10-02 15:05:45 +0200

    Inserted LoginTry handleFunc and modifies login template

--- Unsolved_Problems/trello/pkg/app/routes.go
+++ Unsolved_Problems/trello/pkg/app/routes.go
@@ -3,19 +3,18 @@ package app
 import "net/http"
 
 func (a *App) initializeRoutes() {
-	a.Router.HandleFunc("/",a.Home).Methods("GET")
+	a.Router.HandleFunc("/", a.Home).Methods("GET")
 	a.Router.HandleFunc("/search", a.Search).Methods("GET")
 
-	a.Router.HandleFunc("/css/",serveCSS)
+	a.Router.HandleFunc("/css/", serveCSS)
 
 	a.Router.HandleFunc("/members/{tag}", a.GetMemberByUsername).Methods("GET")
 
+	a.Router.HandleFunc("/board", a.GetBoards).Methods("GET")
+	a.Router.HandleFunc("/board/{id}", a.GetBoardByID).Methods("GET")
 
-	a.Router.HandleFunc("/board",a.GetBoards).Methods("GET")
-	a.Router.HandleFunc("/board/{id}",a.GetBoardByID).Methods("GET")
-
+	a.Router.HandleFunc("/logintry", a.LoginTry).Methods("GET")
 }
-
 
 func serveCSS(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "../tmpl/css/*.css")
