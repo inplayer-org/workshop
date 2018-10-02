5a0488a2b35debe7e2f2a159c5591d75bcf66e36
DIno <dinoshambar@gmail.com>
2018-10-02 16:03:00 +0200

    Created file server with route /img/{imageName}.extension for serving images for the html

--- Unsolved_Problems/trello/pkg/app/routes.go
+++ Unsolved_Problems/trello/pkg/app/routes.go
@@ -10,7 +10,9 @@ func (a *App) initializeRoutes() {
 	a.Router.HandleFunc("/",a.Home).Methods("GET")
 	a.Router.HandleFunc("/search", a.Search).Methods("GET")
 
-	a.Router.HandleFunc("/css/{cssName}",a.serveCSS)
+	a.Router.HandleFunc("/css/{cssName}.css",a.serveCSS)
+	a.Router.HandleFunc("/img/{imageName}.png",a.serveImagePng)
+	a.Router.HandleFunc("/img/{imageName}.jpg",a.serveImageJpg)
 
 	a.Router.HandleFunc("/members/{tag}", a.GetMemberByUsername).Methods("GET")
 
@@ -30,6 +32,34 @@ func (a *App) serveCSS(w http.ResponseWriter, r *http.Request) {
 	log.Println(cssFileName)
 
 	filePath := "../tmpl/css/" + cssFileName + ".css"
+
+	http.ServeFile(w, r, filePath)
+
+}
+
+func (a *App) serveImagePng(w http.ResponseWriter, r *http.Request){
+
+	vars := mux.Vars(r)
+
+	imageFileName := vars["imageName"]
+
+	log.Println(imageFileName,"PNG")
+
+	filePath := "../tmpl/img/" + imageFileName + ".png"
+
+	http.ServeFile(w, r, filePath)
+
+}
+
+func (a *App) serveImageJpg(w http.ResponseWriter, r *http.Request){
+
+	vars := mux.Vars(r)
+
+	imageFileName := vars["imageName"]
+
+	log.Println(imageFileName,"JPG")
+
+	filePath := "../tmpl/img/" + imageFileName + ".jpg"
 
 	http.ServeFile(w, r, filePath)
 
