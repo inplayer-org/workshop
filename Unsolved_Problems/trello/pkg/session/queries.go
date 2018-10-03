a5fddf815454488f1e347480ec6148d1202a1430
Darko Djalevski <dare_zalevski@yahoo.com>
2018-10-03 12:58:54 +0200

    Delete queries from Session

--- Unsolved_Problems/trello/pkg/session/queries.go
+++ Unsolved_Problems/trello/pkg/session/queries.go
@@ -36,9 +36,17 @@ func ( session *Session)  updateByID(DB *sql.DB) error {
 	return err
 }
 
+func (session *Session) Delete (DB *sql.DB) error {
+
+	_, err := DB.Exec("DELETE FROM Trello.Sessions (uid,IDuser) VALUES (?,?);", session.UID, session.IDuser)
+
+	return err
+
+}
 
 
 func GetFromSeassions(db *sql.DB,seasionID string)(Session,error){
+
 
 
 
